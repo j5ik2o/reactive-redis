8ab8a87315ca3dd7425d7673dfa781b523901ecc
Junichi Kato <j5ik2o@gmail.com>
2018-07-12 16:53:42 +0900

    wip

--- core/src/main/scala/com/github/j5ik2o/reactive/redis/RedisClient.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/redis/RedisClient.scala
@@ -4,13 +4,13 @@ import java.util.UUID
 
 import akka.actor.ActorSystem
 import cats.data.ReaderT
-import com.github.j5ik2o.reactive.redis.RedisClient.RC
+import com.github.j5ik2o.reactive.redis.RedisClient.ReaderTTask
 import com.github.j5ik2o.reactive.redis.command._
 import monix.eval.Task
 
 object RedisClient {
 
-  type RC[A] = ReaderT[Task, RedisConnection, A]
+  type ReaderTTask[A] = ReaderT[Task, RedisConnection, A]
 
   def apply()(implicit system: ActorSystem): RedisClient = new RedisClient()
 
@@ -20,20 +20,21 @@ class RedisClient(
     implicit system: ActorSystem
 ) {
 
-  def send[C <: CommandRequest](cmd: C): RC[cmd.Response] = ReaderT(_.send(cmd))
+  def send[C <: CommandRequest](cmd: C): ReaderTTask[cmd.Response] = ReaderT(_.send(cmd))
 
-  def set(key: String, value: String): RC[Unit] = send(SetCommandRequest(UUID.randomUUID(), key, value)).flatMap {
-    case SetSucceeded(_, _) =>
-      ReaderT { _ =>
-        Task.pure(value)
-      }
-    case SetFailed(_, _, ex) =>
-      ReaderT { _ =>
-        Task.raiseError(ex)
-      }
-  }
+  def set(key: String, value: String): ReaderTTask[Unit] =
+    send(SetCommandRequest(UUID.randomUUID(), key, value)).flatMap {
+      case SetSucceeded(_, _) =>
+        ReaderT { _ =>
+          Task.pure(value)
+        }
+      case SetFailed(_, _, ex) =>
+        ReaderT { _ =>
+          Task.raiseError(ex)
+        }
+    }
 
-  def get(key: String): RC[Option[String]] = send(GetCommandRequest(UUID.randomUUID(), key)).flatMap {
+  def get(key: String): ReaderTTask[Option[String]] = send(GetCommandRequest(UUID.randomUUID(), key)).flatMap {
     case GetSucceeded(_, _, value) =>
       ReaderT { _ =>
         Task.pure(value)
