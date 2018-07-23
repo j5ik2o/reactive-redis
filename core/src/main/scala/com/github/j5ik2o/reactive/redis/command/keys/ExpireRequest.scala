ff09e7673935b11041cf14ec1517582a89a25d2d
Junichi Kato <j5ik2o@gmail.com>
2018-07-23 20:53:04 +0900

    fix bugs

--- core/src/main/scala/com/github/j5ik2o/reactive/redis/command/keys/ExpireRequest.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/redis/command/keys/ExpireRequest.scala
@@ -5,7 +5,7 @@ import java.util.UUID
 import com.github.j5ik2o.reactive.redis.RedisIOException
 import com.github.j5ik2o.reactive.redis.command.{ CommandRequest, CommandResponse, StringParsersSupport }
 import com.github.j5ik2o.reactive.redis.parser.StringParsers._
-import com.github.j5ik2o.reactive.redis.parser.model.{ ErrorExpr, Expr, NumberExpr }
+import com.github.j5ik2o.reactive.redis.parser.model.{ ErrorExpr, Expr, NumberExpr, SimpleExpr }
 import fastparse.all._
 
 import scala.concurrent.duration.FiniteDuration
@@ -25,6 +25,8 @@ case class ExpireRequest(id: UUID, key: String, seconds: FiniteDuration)
   override protected def parseResponse: Handler = {
     case (NumberExpr(n), next) =>
       (ExpireSucceeded(UUID.randomUUID(), id, n == 1), next)
+    case (SimpleExpr(QUEUED), next) =>
+      (ExpireSuspended(UUID.randomUUID(), id), next)
     case (ErrorExpr(msg), next) =>
       (ExpireFailed(UUID.randomUUID(), id, RedisIOException(Some(msg))), next)
   }
