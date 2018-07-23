bd21f497ebd96028495278a96afe32a43b87e139
Junichi Kato <j5ik2o@gmail.com>
2018-07-23 19:58:18 +0900

    add tests

--- core/src/test/scala/com/github/j5ik2o/reactive/redis/feature/HashesFeatureSpec.scala
+++ core/src/test/scala/com/github/j5ik2o/reactive/redis/feature/HashesFeatureSpec.scala
@@ -2,7 +2,7 @@ package com.github.j5ik2o.reactive.redis.feature
 
 import akka.actor.ActorSystem
 import akka.routing.DefaultResizer
-import com.github.j5ik2o.reactive.redis.{ AbstractRedisClientSpec, PeerConfig, RedisConnection, RedisConnectionPool }
+import com.github.j5ik2o.reactive.redis._
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
 import org.scalacheck.Shrink
@@ -31,6 +31,16 @@ class HashesFeatureSpec extends AbstractRedisClientSpec(ActorSystem("HashesFeatu
         } yield (n, r))
         result2._1.value shouldBe 1
         result2._2.value shouldBe None
+    }
+    "hexists" in forAll(keyFieldValueGen) {
+      case (k, f, v) =>
+        val result = runProgram(for {
+          _  <- redisClient.hset(k, f, v)
+          r1 <- redisClient.hexists(k, f)
+          _  <- redisClient.hdel(k, f)
+          r2 <- redisClient.hexists(k, f)
+        } yield (r1, r2))
+        result shouldBe (Provided(true), Provided(false))
     }
     "hset & hget" in forAll(keyFieldValueGen) {
       case (k, f, v) =>
