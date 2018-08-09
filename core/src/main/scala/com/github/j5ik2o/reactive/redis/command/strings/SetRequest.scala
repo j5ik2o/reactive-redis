e006aece6410db07b7290409f120cc31578f8ca8
Junichi Kato <j5ik2o@gmail.com>
2018-08-09 10:00:42 +0900

    Update set api.

--- core/src/main/scala/com/github/j5ik2o/reactive/redis/command/strings/SetRequest.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/redis/command/strings/SetRequest.scala
@@ -4,27 +4,32 @@ import java.util.UUID
 
 import cats.Show
 import com.github.j5ik2o.reactive.redis.RedisIOException
-import com.github.j5ik2o.reactive.redis.command.{CommandRequest, CommandResponse, StringParsersSupport}
+import com.github.j5ik2o.reactive.redis.command.{ CommandRequest, CommandResponse, StringParsersSupport }
 import com.github.j5ik2o.reactive.redis.parser.StringParsers._
-import com.github.j5ik2o.reactive.redis.parser.model.{ErrorExpr, Expr, SimpleExpr}
+import com.github.j5ik2o.reactive.redis.parser.model.{ ErrorExpr, Expr, SimpleExpr }
 import enumeratum._
 import fastparse.all._
 
 import scala.collection.immutable
 import scala.concurrent.duration.Duration
 
-sealed trait SetExpiration{ val duration: Duration }
+sealed trait SetExpiration { val duration: Duration }
 final case class SetExExpiration(duration: Duration) extends SetExpiration
 final case class SetPxExpiration(duration: Duration) extends SetExpiration
-sealed trait SetOption extends EnumEntry
+sealed trait SetOption                               extends EnumEntry
 object SetOption extends Enum[SetOption] {
   override def values: immutable.IndexedSeq[SetOption] = findValues
   case object NX extends SetOption
   case object XX extends SetOption
 }
 
-final case class SetRequest(id: UUID, key: String, value: String, expiration: Option[SetExpiration], setOption: Option[SetOption])
-  extends CommandRequest with StringParsersSupport {
+final case class SetRequest(id: UUID,
+                            key: String,
+                            value: String,
+                            expiration: Option[SetExpiration],
+                            setOption: Option[SetOption])
+    extends CommandRequest
+    with StringParsersSupport {
 
   override type Response = SetResponse
 
@@ -36,7 +41,9 @@ final case class SetRequest(id: UUID, key: String, value: String, expiration: Op
   }
 
   override def asString: String =
-    s"""SET $key "$value"${expiration.fold("")(v => s" ${setExpirationToString(v)}")}${setOption.fold("")(v => s" ${v.entryName}")}"""
+    s"""SET $key "$value"${expiration.fold("")(v => s" ${setExpirationToString(v)}")}${setOption.fold("")(
+      v => s" ${v.entryName}"
+    )}"""
 
   override protected lazy val responseParser: P[Expr] = fastParse(simpleStringReply | errorReply)
 
@@ -53,7 +60,11 @@ final case class SetRequest(id: UUID, key: String, value: String, expiration: Op
 
 object SetRequest {
 
-  def apply[A](id: UUID, key: String, value: A, expiration: Option[SetExpiration] = None, setOption: Option[SetOption] = None)(implicit s: Show[A]): SetRequest =
+  def apply[A](id: UUID,
+               key: String,
+               value: A,
+               expiration: Option[SetExpiration] = None,
+               setOption: Option[SetOption] = None)(implicit s: Show[A]): SetRequest =
     new SetRequest(id, key, s.show(value), expiration, setOption)
 
 }
