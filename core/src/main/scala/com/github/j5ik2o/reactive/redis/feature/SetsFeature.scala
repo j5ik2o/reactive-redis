dbcc40b7503596ec03fdff6a69d5e18b09667581
Junichi Kato <j5ik2o@gmail.com>
2018-08-09 10:07:08 +0900

    add SetsAPI trait

--- core/src/main/scala/com/github/j5ik2o/reactive/redis/feature/SetsFeature.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/redis/feature/SetsFeature.scala
@@ -6,12 +6,17 @@ import cats.data.NonEmptyList
 import com.github.j5ik2o.reactive.redis._
 import com.github.j5ik2o.reactive.redis.command.sets.{ SAddFailed, SAddRequest, SAddSucceeded, SAddSuspended }
 
-trait SetsFeature { this: RedisClient =>
+trait SetsAPI[M[_]] {
+  def sAdd(key: String, member: String, members: String*): M[Result[Long]]
+  def sAdd(key: String, members: NonEmptyList[String]): M[Result[Long]]
+}
 
-  def sAdd(key: String, member: String, members: String*): ReaderTTaskRedisConnection[Result[Long]] =
+trait SetsFeature extends SetsAPI[ReaderTTaskRedisConnection] { this: RedisClient =>
+
+  override def sAdd(key: String, member: String, members: String*): ReaderTTaskRedisConnection[Result[Long]] =
     sAdd(key, NonEmptyList.of(member, members: _*))
 
-  def sAdd(key: String, members: NonEmptyList[String]): ReaderTTaskRedisConnection[Result[Long]] =
+  override def sAdd(key: String, members: NonEmptyList[String]): ReaderTTaskRedisConnection[Result[Long]] =
     send(SAddRequest(UUID.randomUUID(), key, members)).flatMap {
       case SAddSuspended(_, _)        => ReaderTTask.pure(Suspended)
       case SAddSucceeded(_, _, value) => ReaderTTask.pure(Provided(value))
