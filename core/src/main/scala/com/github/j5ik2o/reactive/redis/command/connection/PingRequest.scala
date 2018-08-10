87965c800ecfcb696341831c2b1dbedaa645de06
Junichi Kato <j5ik2o@gmail.com>
2018-08-10 10:35:46 +0900

    add buildCmdString method

--- core/src/main/scala/com/github/j5ik2o/reactive/redis/command/connection/PingRequest.scala
+++ core/src/main/scala/com/github/j5ik2o/reactive/redis/command/connection/PingRequest.scala
@@ -17,7 +17,7 @@ final case class PingRequest(id: UUID, message: Option[String] = None)
 
   override val isMasterOnly: Boolean = true
 
-  override def asString: String = s"PING${message.fold("")(v => s" $v")}"
+  override def asString: String = buildCmdString("PING", message)
 
   override protected lazy val responseParser: P[Expr] = fastParse(bulkStringReply | simpleStringReply | errorReply)
 
